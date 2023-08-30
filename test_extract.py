6770e912beaa38ed4fd02a3d950c16334af8499d
Hassan Kashif <hassankashif@Hassans-MacBook-Pro-2.local>
2023-08-30 13:58:38 +0100

    Added conftest file, 1 test to extract, 2 tests to transform

--- test_extract.py
+++ test_extract.py
@@ -15,82 +15,46 @@ from extract import (
     create_json_file
 )
 
-# load_dotenv()
+load_dotenv()
 
-# api_path = environ.get("API_PATH")
+api_path = environ.get("API_PATH")
 
-# all_plants_data = get_all_plants_data(api_path)
-
-
-# def is_valid_timestamp(timestamp_str: str) -> bool:
-#     """
-#     Verifies whether a given
-#     string is a valid timestamp
-#     """
-#     try:
-#         parse(timestamp_str)
-#         return True
-#     except ValueError:
-#         return False
+all_plants_data = get_all_plants_data(api_path)
 
 
-# def test_get_plant_data_produces_dict():
-#     """
-#     Verifies that get_plant_data()
-#     produces a dictionary per plant
-#     """
-#     plant_data_1 = get_plant_data(1, api_path)
-#     assert isinstance(plant_data_1, dict)
+def is_valid_timestamp(timestamp_str: str) -> bool:
+    """
+    Verifies whether a given
+    string is a valid timestamp
+    """
+    try:
+        parse(timestamp_str)
+        return True
+    except ValueError:
+        return False
 
-#     for plant in all_plants_data:
-#         assert isinstance(plant, dict)
+
+def test_get_plant_data_produces_dict():
+    """
+    Verifies that get_plant_data()
+    produces a dictionary per plant
+    """
+    plant_data_1 = get_plant_data(1, api_path)
+    assert isinstance(plant_data_1, dict)
+
+    for plant in all_plants_data:
+        assert isinstance(plant, dict)
+
 
 @patch("requests.get")
-def test_get_plant_data_calls_api(mock_get):
+def test_get_plant_data_calls_api(mock_get, mock_api_data, mock_nested_data):
     """
     Test `get_plant_data` to see if the `requests.get` function is called correctly
     """
     mock_response = MagicMock()
-    mock_response.json.return_value = {
-        "plant_id": 1,
-        "name": "Mock Scientific Name"
-        "scientific_name": [
-            "Mock Scientific Name"
-        ],
-        "cycle": "Mock Cycle",
-        "last_watered": "Mon, 1 Jan 2023 00:00:00 GMT",
-        "recording_time": "2023-01-01 00:00:00",
-        "temperature": 0,
-        "soil_moisture": 0,
-        "sunlight_details": [
-            "Mock detail",
-            "Mock detail"
-        ],
-        "origin_location": {
-            "origin_latitude": "5.27247",
-            "origin_longitude": "-3.59625",
-            "origin_country": "Mock Country"
-        },
-        "botanist_details": {
-            "email": "mock@example.com",
-            "name": "Mock Botanist",
-            "phone": "+000-000-000.000x000mock"
-        }
-    }
-    [
-    {"botanist": {"email": "mock@example.com", "name": "Mock Botanist", "phone": "+000-000-000.000x000mock"},
-    "cycle": "Mock Cycle", 
-    "last_watered": "Mon, 1 Jan 2023 00:00:00 GMT",
-    "name": "Mock Scientific Name"
-    "origin_location": ["5.27247", "-3.59625", "Bonoua", "CI", "Africa/Abidjan"], 
-    "plant_id": 8, 
-    "recording_taken": "2023-08-29 11:12:20", 
-    "scientific_name": ["Mock Scientific Name"], 
-    "soil_moisture": 21.909097507952296, 
-    "sunlight": ["Full sun", "part shade"], 
-    "temperature": 11.4968735605431
-    }
-]
+    mock_response.json.return_value = mock_api_data
+
+    expected_result = mock_nested_data
 
     mock_get.return_value = mock_response
     mock_id = 1
@@ -99,57 +63,58 @@ def test_get_plant_data_calls_api(mock_get):
 
     assert mock_get.call_count == 1
     assert mock_response.json.call_count == 1
+    assert result == expected_result
 
 
-# def test_get_plant_data_has_correct_information():
-#     """
-#     Tests that get_plant_data()
-#     is assigning the correct values
-#     to the correct keys
-#     """
-#     plant_data_1 = get_plant_data(1, api_path)
-#     assert plant_data_1["name"].lower() == "venus flytrap"
+def test_get_plant_data_has_correct_information():
+    """
+    Tests that get_plant_data()
+    is assigning the correct values
+    to the correct keys
+    """
+    plant_data_1 = get_plant_data(1, api_path)
+    assert plant_data_1["name"].lower() == "venus flytrap"
 
-#     plant_data_17 = get_plant_data(17, api_path)
-#     assert plant_data_17["name"].lower() == "ipomoea batatas"
+    plant_data_17 = get_plant_data(17, api_path)
+    assert plant_data_17["name"].lower() == "ipomoea batatas"
 
-#     for plant in all_plants_data:
-#         if plant.get("recording_taken"):
-#             validate_timestamp = is_valid_timestamp(plant["recording_taken"])
-#             assert validate_timestamp == True
-
-
-# def test_data_is_retrieved_for_all_plants():
-#     """
-#     Tests that get_all_plants_data()
-#     is a list that contains information
-#     on each plant in the exhibit
-#     """
-#     assert isinstance(all_plants_data, list)
-#     assert len(all_plants_data) == 51
+    for plant in all_plants_data:
+        if plant.get("recording_taken"):
+            validate_timestamp = is_valid_timestamp(plant["recording_taken"])
+            assert validate_timestamp == True
 
 
-# def test_create_json_file():
+def test_data_is_retrieved_for_all_plants():
+    """
+    Tests that get_all_plants_data()
+    is a list that contains information
+    on each plant in the exhibit
+    """
+    assert isinstance(all_plants_data, list)
+    assert len(all_plants_data) == 51
 
-#     test_data = [
-#         {"id": 1, "name": "daisy"},
-#         {"id": 2, "name": "marigold"}
-#     ]
 
-#     with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-#         temp_file_path = temp_file.name
+def test_create_json_file():
 
-#     result = create_json_file(test_data, temp_file_path)
+    test_data = [
+        {"id": 1, "name": "daisy"},
+        {"id": 2, "name": "marigold"}
+    ]
 
-#     assert result == "Data processed!"
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_file_path = temp_file.name
 
-#     with open(temp_file_path, "r") as json_file:
-#         file_content = json_file.read()
-#         actual_data = json.loads(file_content)
-#         expected_data = [
-#             {"id": 1, "name": "daisy"},
-#             {"id": 2, "name": "marigold"}
-#         ]
-#         assert actual_data == expected_data
+    result = create_json_file(test_data, temp_file_path)
 
-#     os.remove(temp_file_path)
+    assert result == "Data processed!"
+
+    with open(temp_file_path, "r") as json_file:
+        file_content = json_file.read()
+        actual_data = json.loads(file_content)
+        expected_data = [
+            {"id": 1, "name": "daisy"},
+            {"id": 2, "name": "marigold"}
+        ]
+        assert actual_data == expected_data
+
+    os.remove(temp_file_path)
--- test_transform.py
+++ test_transform.py
@@ -1,1 +1,36 @@
 """Test Script: Testing functions from transform.py"""
+from unittest.mock import MagicMock, patch
+import pandas as pd
+
+from transform import (
+    flatten_data,
+    transform_email_column_using_regex
+)
+
+
+def test_flatten_data_returns_correct_data(mock_nested_data, mock_flattened_data):
+    """"""
+    result = flatten_data([mock_nested_data])
+    assert result == mock_flattened_data
+
+
+def test_transform_email_column_using_regex():
+    mock_email_data = {
+        "botanist_email": [
+            "mockOne@example.com",
+            "mockTwo@example.com Mock",
+            "Mock mockThree@example.com",
+            "Mock mockFour@example.com Mock"
+        ]
+    }
+    mock_database = pd.DataFrame(mock_email_data)
+    result_df = transform_email_column_using_regex(mock_database)
+
+    expected_result = [
+        "mockOne@example.com",
+        "mockTwo@example.com",
+        "mockThree@example.com",
+        "mockFour@example.com"
+    ]
+
+    assert result_df["botanist_email"].tolist() == expected_result

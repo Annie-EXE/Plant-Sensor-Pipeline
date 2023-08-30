a447269d5aa1a98fc045a1f62b48a579d5aa2958
Simeon Adesanya <simeon_a@Simeons-MacBook-Pro.local>
2023-08-30 09:25:00 +0100

    Create base load file

--- load.py
+++ load.py
@@ -20,5 +20,80 @@ def get_db_connection() -> connection:
                    port=config["db_port"])
 
 
+# TODO Typehint for data?
+def insert_into_plant_table(conn: connection, data) -> None:
+    """Inserts information into plant table"""
+
+    cur = conn.cursor()
+
+    cur.execute("""INSERT INTO plant
+                (plant_name,
+                plant_scientific_name,
+                plant_origin,
+                water_history_id)
+                Values
+                (%s, %s, %s, %s);
+                """([data]))
+
+    cur.commit()
+
+
+def insert_into_plant_origin_table(conn: connection, data) -> None:
+    """Inserts information into plant_origin table"""
+
+    cur = conn.cursor()
+
+    cur.execute("""INSERT INTO plant_origin
+                (latitude, longitude, country)
+                Values
+                (%s, %s, %s);
+                """([data]))
+
+    cur.commit()
+
+
+def insert_into_botanist_table(conn: connection, data) -> None:
+    """Inserts information into botanist table"""
+
+    cur = conn.cursor()
+
+    cur.execute("""INSERT INTO botanist
+                (botanist_name, botanist_email, botanist_phone_number)
+                Values
+                (%s, %s, %s);
+                """([data]))
+
+    cur.commit()
+
+
+def insert_into_water_history_table(conn: connection, data) -> None:
+    """Inserts information into water_history table"""
+
+    cur = conn.cursor()
+
+    cur.execute("""INSERT INTO water_history
+                (water_history_id, time_watered)
+                Values
+                (%s, %s);
+                """([data]))
+
+    cur.commit()
+
+
+def insert_into_reading_information_table(conn: connection, data) -> None:
+    """Inserts information into reading_information table"""
+
+    cur = conn.cursor()
+
+    cur.execute("""INSERT INTO reading_information
+                (plant_id, plant_reading_time, botanist_id,
+                temperature, soil_moisture, sunlight_id)
+                Values
+                (%s, %s, %s, %s, %s, %s);
+                """([data]))
+
+    cur.commit()
+
+
 if __name__ == "__main__":
     pass

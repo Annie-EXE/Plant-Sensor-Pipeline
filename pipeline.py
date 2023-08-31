b6bd429b742af5c12cf9a463dffcea52ffb8e479
Hassan Kashif <hassankashif@Hassans-MacBook-Pro-2.local>
2023-08-31 15:20:30 +0100

    Reorganising pipeline functions

--- pipeline.py
+++ pipeline.py
@@ -31,25 +31,18 @@ if __name__ == "__main__":
 
     api_path = environ.get("API_PATH")
 
-
     all_plants_data = get_all_plants_data(api_path)
-
     cleaned_plants_data = clean_unicode_from_plant_data(all_plants_data)
 
     flatted_plant_data = flatten_data(cleaned_plants_data)
-
     plant_df = build_plant_dataframe(flatted_plant_data)
 
     conn = get_db_connection(config)
 
     insert_into_plant_origin_table(conn, plant_df)
-
     insert_into_plant_table(conn, plant_df)
-
     insert_into_botanist_table(conn, plant_df)
-
     insert_into_water_history_table(conn, plant_df)
-
     insert_into_reading_information_table(conn, plant_df)
 
     conn.close()

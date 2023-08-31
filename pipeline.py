bfc6254e79d7495f7168cf9bb94843943ae7c8f0
Hassan Kashif <hassankashif@Hassans-MacBook-Pro-2.local>
2023-08-31 15:18:31 +0100

    Added environment to README

--- pipeline.py
+++ pipeline.py
@@ -7,33 +7,49 @@ from dotenv import load_dotenv
 from extract import (
     get_all_plants_data,
     clean_unicode_from_plant_data,
-    create_json_file
 )
 
 from transform import (
-    load_data,
     flatten_data,
     build_plant_dataframe
+)
+
+from load import (
+    get_db_connection,
+    insert_into_plant_origin_table,
+    insert_into_plant_table,
+    insert_into_botanist_table,
+    insert_into_water_history_table,
+    insert_into_reading_information_table
 )
 
 if __name__ == "__main__":
 
     load_dotenv()
 
+    config = environ
+
     api_path = environ.get("API_PATH")
 
-    plant_data_file_path = "recent_plant_data.json"
 
     all_plants_data = get_all_plants_data(api_path)
 
     cleaned_plants_data = clean_unicode_from_plant_data(all_plants_data)
 
-    create_json_file(all_plants_data, plant_data_file_path)
-
-    loaded_data_from_file = load_data(plant_data_file_path)
-
-    flatted_plant_data = flatten_data(loaded_data_from_file)
+    flatted_plant_data = flatten_data(cleaned_plants_data)
 
     plant_df = build_plant_dataframe(flatted_plant_data)
 
-    print(plant_df)
+    conn = get_db_connection(config)
+
+    insert_into_plant_origin_table(conn, plant_df)
+
+    insert_into_plant_table(conn, plant_df)
+
+    insert_into_botanist_table(conn, plant_df)
+
+    insert_into_water_history_table(conn, plant_df)
+
+    insert_into_reading_information_table(conn, plant_df)
+
+    conn.close()
